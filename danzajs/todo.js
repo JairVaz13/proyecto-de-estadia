16a547958e2d7c274f3275336a031914ab0a11ba
JairVaz13 <1722110433@utectulancingo.edu.mx>
2024-08-19 13:05:08 -0600

    commit

--- danzajs/todo.js
+++ danzajs/todo.js
@@ -1,9 +1,6 @@
-const NEWS_API_URL = 'http://localhost:8000/news';
-const VIDEOS_API_URL = 'http://localhost:8000/videos';
+const NEWS_API_URL = 'https://api2-estadia.onrender.com/news';
 const NEWS_PER_PAGE = 4;
-const VIDEOS_PER_PAGE = 3;
 let currentNewsPage = 1;
-let currentVideoPage = 0;
 let totalNewsPages = 1;
 
 function fetchNews() {
@@ -63,41 +60,4 @@ function changeNewsPage(pageNumber) {
     fetchNews();
 }
 
-async function fetchVideos(page = 0) {
-    const skip = page * VIDEOS_PER_PAGE;
-    try {
-        const response = await fetch(`${VIDEOS_API_URL}?skip=${skip}&limit=${VIDEOS_PER_PAGE}`);
-        const videos = await response.json();
-        const videoGrid = document.getElementById('videoGrid');
-        videoGrid.innerHTML = '';
-        videos.forEach(video => {
-            const card = document.createElement('div');
-            card.className = 'col-md-4';
-            card.innerHTML = `
-                <div class="card video-card">
-                    <video class="card-img-top" controls>
-                        <source src="${video.video_url}" type="video/mp4">
-                        Your browser does not support the video tag.
-                    </video>
-                    <div class="card-body">
-                        <h5 class="card-title">${video.title}</h5>
-                        <p class="card-text">${video.description}</p>
-                    </div>
-                </div>
-            `;
-            videoGrid.appendChild(card);
-        });
-        document.getElementById('prevPage').disabled = page === 0;
-        document.getElementById('nextPage').disabled = videos.length < VIDEOS_PER_PAGE;
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
-function changeVideoPage(delta) {
-    currentVideoPage += delta;
-    fetchVideos(currentVideoPage);
-}
-
 fetchNews();
-fetchVideos(currentVideoPage);
